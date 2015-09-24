cd950056035435bdac36727f9b611783aec2130b
wesley974 <milo974@gmail.com>
2015-09-24 15:58:28 +0400

    Add comment

--- install/install.sh
+++ install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Last changes : 2015/09/24, Wesley MOUEDINE ASSABY
 # Contact me :  wesley at mouedine dot net
-# MISSING PARTS : CRON / NGINX / MONGREL / RUBY ON RAILS APPS 
+# MISSING PARTS : CRON / NGINX / MONGREL / RUBY ON RAILS APPS / ROUNDCUBE
 
 TEMPLATES=/var/mailserv/install/templates
 
